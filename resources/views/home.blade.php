201b617d2c99da045f914ec851c36e65950a0271
yahyaBir <yahyabirr61@gmail.com>
2025-01-29 23:10:42 +0300

    Product card tasarımı güncellendi, hover efektleri eklendi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,11 +19,10 @@
             @for ($i = 1; $i <= 4; $i++)
                 <div class="product-card">
                     <div class="product-image">
-                        <img src="{{ asset('images/product-' . $i . '.jpg') }}" alt="Süperkritik Ekstraktör">
+                        <img src="{{ asset('images/robomation.png') }}" alt="Süperkritik Ekstraktör">
                     </div>
                     <div class="product-info">
                         <h3>Süperkritik Ekstraktör</h3>
-                        <a href="#" class="product-link">Detaylı Bilgi</a>
                     </div>
                 </div>
             @endfor
